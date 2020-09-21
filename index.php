c34d51550fffc8121e0e284f38891cb464be1af3
Dileep Kumar Meghwal <Dileepmeghwal@users.noreply.github.com>
2020-09-21 12:12:59 +0530

    Teletronics Web Application
    Based on Online Data Management Syastem

---
+++ index.php
@@ -0,0 +1,196 @@
+ <!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+
+  <!-- Font Awesome CSS -->
+  <link rel="stylesheet" href="css/all.min.css">
+
+  <!-- Google Font -->
+  <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+
+  <!-- Custom CSS -->
+  <link rel="stylesheet" href="css/custom.css">
+
+  <title>Telectronics</title>
+</head>
+
+<body>
+  <!-- Start Navigation -->
+  <nav class="navbar navbar-expand-sm navbar-dark bg-danger pl-5 fixed-top">
+    <a href="index.php" class="navbar-brand">Telectronics</a>
+    <span class="navbar-text">Customer's Happiness is our Aim</span>
+    <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#myMenu">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="myMenu">
+      <ul class="navbar-nav pl-5 custom-nav">
+        <li class="nav-item"><a href="index.php" class="nav-link">Home</a></li>
+        <li class="nav-item"><a href="#Services" class="nav-link">Services</a></li>
+        <li class="nav-item"><a href="#registration" class="nav-link">Registration</a></li>
+        <li class="nav-item"><a href="Requester/RequesterLogin.php" class="nav-link">Login</a></li>
+        <li class="nav-item"><a href="#Contact" class="nav-link">Contact</a></li>
+      </ul>
+    </div>
+  </nav> <!-- End Navigation -->
+
+  <!-- Start Header Jumbotron-->
+  <header class="jumbotron back-image" style="background-image: url(images/imgbg.jpg);">
+    <div class="myclass mainHeading">
+      <h1 class="text-uppercase text-danger font-weight-bold">Welcome to Telectronics</h1>
+      <p class="font-italic">Customer's Happiness is our Aim</p>
+      <a href="Requester/RequesterLogin.php" class="btn btn-success mr-4">Login</a>
+      <a href="#registration" class="btn btn-danger mr-4">Sign Up</a>
+    </div>
+  </header> <!-- End Header Jumbotron -->
+
+  <div class="container">
+    <!--Introduction Section-->
+    <div class="jumbotron">
+      <h3 class="text-center">Telectronics Services</h3>
+      <p>
+        Telectronics Services is India’s leading chain of multi-brand Electronics and Electrical service
+        workshops offering
+        wide array of services. We focus on enhancing your uses experience by offering world-class
+        Electronic
+        Appliances maintenance services. Our sole mission is “To provide Electronic Appliances care
+        services to
+        keep the devices fit and healthy and customers happy and smiling”.
+
+        With well-equipped Electronic Appliances service centres and fully trained mechanics, we
+        provide quality
+        services with excellent packages that are designed to offer you great savings.
+
+        Our state-of-art workshops are conveniently located in many cities across the country. Now you
+        can book
+        your service online by doing Registration.
+      </p>
+
+    </div>
+  </div>
+  <!--Introduction Section End-->
+  <!-- Start Services -->
+  <div class="container text-center border-bottom" id="Services">
+    <h2>Our Services</h2>
+    <div class="row mt-4">
+      <div class="col-sm-4 mb-4">
+        <a href="#"><i class="fas fa-tv fa-8x text-success"></i></a>
+        <h4 class="mt-4">Electronic Appliances</h4>
+      </div>
+      <div class="col-sm-4 mb-4">
+        <a href="#"><i class="fas fa-sliders-h fa-8x text-primary"></i></a>
+        <h4 class="mt-4">Preventive Maintenance</h4>
+      </div>
+      <div class="col-sm-4 mb-4">
+        <a href="#"><i class="fas fa-cogs fa-8x text-info"></i></a>
+        <h4 class="mt-4">Fault Repair</h4>
+      </div>
+    </div>
+  </div> <!-- End Services -->
+
+  <!-- Start Registration  -->
+  <?php include('userRegistration.php') ?>
+  <!-- End Registration  -->
+
+  <!-- Start Happy Customer  -->
+  <div class="jumbotron bg-danger" id="Customer">
+    <!-- Start Happy Customer Jumbotron -->
+    <div class="container">
+      <!-- Start Customer Container -->
+      <h2 class="text-center text-white">Happy Customers</h2>
+      <div class="row mt-5">
+        <div class="col-lg-3 col-sm-6">
+          <!-- Start Customer 1st Column-->
+          <div class="card shadow-lg mb-2">
+            <div class="card-body text-center">
+              <img src="images/love_jain.jpg" class="img-fluid" style="border-radius: 100px;">
+              <h4 class="card-title">Love Jain</h4>
+              <p class="card-text">B.Tech<br>Computer Engineer</p>
+            </div>
+          </div>
+        </div> <!-- End Customer 1st Column-->
+
+        <div class="col-lg-3 col-sm-6">
+          <!-- Start Customer 2nd Column-->
+          <div class="card shadow-lg mb-2">
+            <div class="card-body text-center">
+              <img src="images/x.jpg" class="img-fluid" style="border-radius: 100px;">
+              <h4 class="card-title">Dileep Meghwal</h4>
+              <p class="card-text">B.Tech<br>Computer Engineer</p>
+            </div>
+          </div>
+        </div> <!-- End Customer 2nd Column-->
+
+        <div class="col-lg-3 col-sm-6">
+          <!-- Start Customer 3rd Column-->
+          <div class="card shadow-lg mb-2">
+            <div class="card-body text-center">
+              <img src="images/Kuldeep.jpg" class="img-fluid" style="border-radius: 100px;">
+              <h4 class="card-title">Kuldeep Prajapat</h4>
+              <p class="card-text">B.Tech<br>Computer Engineer</p>
+            </div>
+          </div>
+        </div> <!-- End Customer 3rd Column-->
+
+        <div class="col-lg-3 col-sm-6">
+          <!-- Start Customer 4th Column-->
+          <div class="card shadow-lg mb-2">
+            <div class="card-body text-center">
+              <img src="images/rajesh.jpg" class="img-fluid" style="border-radius: 100px;">
+              <h4 class="card-title">Rajesh Garasiya</h4>
+              <p class="card-text">B.Tech<br>Computer Engineer</p>
+            </div>
+          </div>
+        </div> <!-- End Customer 4th Column-->
+      </div> <!-- End Customer Row-->
+    </div> <!-- End Customer Container -->
+  </div> <!-- End Customer Jumbotron -->
+
+  <!--Start Contact Us-->
+  <div class="container" id="Contact">
+    <!--Start Contact Us Container-->
+    <h2 class="text-center mb-4">Contact US</h2> <!-- Contact Us Heading -->
+    <div class="row">
+
+      <!--Start Contact Us Row-->
+      <?php include('contactform.php'); ?>
+      <!-- End Contact Us 1st Column -->
+
+      <div class="col-md-4 text-center">
+        <!-- Start Contact Us 2nd Column-->
+        <strong>Headquarter:</strong> <br>
+        Telectronics Pvt Ltd, <br>
+        Hiran Magri Sec IV, Menariya guest 
+        House <br>
+        Rajasathan - 313803 <br>
+        Phone: +918696313143 <br>
+        <a href="#" target="_blank">www.Telectronics.com</a> <br>
+
+        <br><br>
+        <strong>Delhi Branch:</strong> <br>
+        Telectronics Pvt Ltd, <br>
+        Ashok Nagar, Delhi <br>
+        Delhi - 804002 <br>
+        Phone: +00000000 <br>
+        <a href="#" target="_blank">www.Telectronics.com</a> <br>
+      </div> <!-- End Contact Us 2nd Column-->
+    </div> <!-- End Contact Us Row-->
+  </div> <!-- End Contact Us Container-->
+  <!-- End Contact Us -->
+
+  <!-- Start Footer-->
+  <footer class="container-fluid bg-dark text-white mt-5" style="border-top: 3px solid #DC3545;">
+    <div class="container">
+      <!-- Start Footer Container -->
+      <div class="row py-3">
+        <!-- Start Footer Row -->
+        <div class="col-md-6">
+          <!-- Start Footer 1st Column -->
+          <span class="pr-2">Follow Us: </span>
+          <a href="#" target="_blank" class="pr-2 fi-color"><i class="fab fa-facebook-f"></i></a>
+          <a href="#" target="_blank" class="pr-2 fi-color"><i class="fab fa-twitter"></i
